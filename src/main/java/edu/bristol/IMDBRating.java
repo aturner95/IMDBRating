95fb0bb7127c26bdebfb0b7d3a064eb7d2e66f39
drew <andrewturner10@hotmail.com>
2022-02-22 10:35:58 +0000

    attempt #1 at worksheet

--- src/main/java/edu/bristol/IMDBRating.java
+++ src/main/java/edu/bristol/IMDBRating.java
@@ -1,5 +1,13 @@
 package edu.bristol;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class IMDBRating
 {
     private float currentAverage = 0;
@@ -7,10 +15,38 @@ public class IMDBRating
 
     public float addNewRating(int newRating)
     {
+        if(newRating < 0 || newRating > 10){
+            return currentAverage;
+        }
+
         int previousTotal = (int) (currentAverage * ratingCount);
         int newTotal = previousTotal + newRating;
         ratingCount++;
         currentAverage = newTotal / ratingCount;
         return currentAverage;
     }
+
+        public boolean checkResultsContain(String epName) throws IOException {
+            URL url = new URL("https://www.imdb.com/search/title/?locations=bristol&role=nm0263368");
+            HttpURLConnection connection =(HttpURLConnection)url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+            InputStream stream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            String nextLine = reader.readLine();
+            int numOfShoestringEps = 0;
+            while(nextLine != null) {
+                // Check the line to see what it contains
+                // ?????????????????????????????????????????
+
+                if(nextLine.contains(epName)){
+                    return true;
+                }
+
+                nextLine = reader.readLine();
+            }
+            return false;
+        }
+
+
 }
--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -1,6 +1,8 @@
 package edu.bristol;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IMDBRatingTest
@@ -16,5 +18,167 @@ public class IMDBRatingTest
 
         averageRating = rater.addNewRating(4);
         assertTrue(averageRating == 3.0, "Adding 2nd rating: average should be 3.0");
+
+        // happy path tests
+        averageRating = rater.addNewRating(6);
+        assertTrue(averageRating == 4.0, "Adding 3rd rating: average should be 4.0");
+
+        averageRating = rater.addNewRating(8);
+        assertTrue(averageRating == 5.0, "Adding 4th rating: average should be 5.0");
+
+
+        // unhappy tests
+        averageRating = rater.addNewRating(-1);
+        assertTrue(averageRating == 5.0, "Adding 5th rating: average should be 5.0");
+
+        averageRating = rater.addNewRating(15);
+        assertTrue(averageRating == 5.0, "Adding 6th rating: average should be 5.0");
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep1() throws Exception{
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Private Ear"));
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep2() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Knock for Knock"));
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep3() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Higher Ground"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep4() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("An Uncertain Circle"));
+
+    }
+
+
+    @Test
+    public void test_checkResultsContainS1Ep5() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Listen to Me"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep6() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Nine Tenths of the Law"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep7() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("The Link-Up"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep8() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Stamp Duty"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep9() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Find the Lady"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep10() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("The Partnership"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS1Ep11() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("I’m a Believer"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep1() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Room with a View"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep2() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("The Teddy Bears’ Nightmare"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep3() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Mocking Bird"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep4() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("The Mayfly Dance"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep5() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("The Farmer Had a Wife"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep6() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Utmost Good Faith"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep7() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Looking for Mr Wright"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep8() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Another Man’s Castle"));
+
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep9() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("Where Was I?"));
+
+    }
+
+    @Test
+    public void test_checkResultsContainS2Ep10() throws Exception {
+        IMDBRating obj = new IMDBRating();
+        assertTrue(obj.checkResultsContain("The Dangerous Game"));
     }
 }
